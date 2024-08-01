b404eaaa1168d9b34e7f3511d7e6aa476c264660
Geethaka Sankalpa <geethaka.20231659@iit.ac.lk>
2024-08-01 20:42:19 +0530

    updated to get the city name

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const apiKey = "ae042a47d8b750c31ae18b3bc2e2e7a6";
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
 
-const searchBox = document.querySelector(".search input");
+const searchBox = document.querySelector(".search input");  // selects the input field 
+const searchButton = document.querySelector(".search button");  // when search button is selected, the city name should be passed to method
 
 async function checkWeather(city) {
     const response = await fetch(apiURL + city + `&appid=${apiKey}`);
@@ -25,4 +26,6 @@ async function checkWeather(city) {
   document.querySelector(".wind").innerHTML= data.wind.speed + 'km/h';     // updates wind speed
 }
 
-checkWeather();
+searchButton.addEventListener("click", ()=> {
+    checkWeather(searchBox.value);
+})
